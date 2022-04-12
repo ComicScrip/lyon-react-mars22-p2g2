12602b6a30f3a062c912d0382c1306b7261628a1
RayaneArroudj <rayane.arroudj@gmail.com>
2022-04-12 17:28:27 +0200

    Navbar en cours

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,62 @@
+import { useEffect, useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './header.css';
 
 export default function Header() {
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const [largeur, setLargeur] = useState(window.innerWidth);
+
+  const toggleNavSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  useEffect(() => {
+    const changeWidth = () => {
+      setLargeur(window.innerWidth);
+      if (window.innerWidth > 500) {
+        setToggleMenu(false);
+      }
+    };
+    window.addEventListener('resize', changeWidth);
+
+    return () => {
+      window.removeEventListener('resize', changeWidth);
+    };
+  }, []);
+
   return (
-    <nav>
-      <ul className="liste">
-        <li className="items">Home</li>
-        <li className="items">Profil</li>
-        <li className="items">Quizz</li>
-        <li className="items">Contact</li>
-      </ul>
-      <button type="button" className="btn">
-        btn
+    <nav className="nav">
+      {(toggleMenu || largeur > 500) && (
+        <div className="liste">
+          <NavLink
+            className={'item'}
+            to="../routes/Home"
+            style={({ isActive }) => ({ color: isActive ? 'orange' : 'white' })}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            to="../routes/Quizz"
+            style={({ isActive }) => ({ color: isActive ? 'orange' : 'white' })}
+          >
+            Quizz
+          </NavLink>
+          <NavLink
+            to="../routes/Profil"
+            style={({ isActive }) => ({ color: isActive ? 'orange' : 'white' })}
+          >
+            Profil
+          </NavLink>
+          <NavLink
+            to="../routes/Home"
+            style={({ isActive }) => ({ color: isActive ? 'orange' : 'white' })}
+          >
+            Contact
+          </NavLink>
+        </div>
+      )}
+      <button onClick={toggleNavSmallScreen} type="button" className="btn">
+        Btn
       </button>
     </nav>
   );
