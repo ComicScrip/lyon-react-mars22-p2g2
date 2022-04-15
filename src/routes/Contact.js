f9987b0849f3b0b90e3eeb0af44d624c7da0e247
mahamat <mahamatadnan@gmail.com>
2022-04-15 14:07:47 +0200

    Possibilité de pouvoir écrire un message dans la partie indiquée

--- src/routes/Contact.js
+++ src/routes/Contact.js
@@ -1,3 +1,58 @@
-export default function Contact() {
-  return <div>Contact</div>;
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import styles from './ContactPage.module.css';
+
+export default function ContactPage() {
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    toast('Merci ! Nous reviendrons vite vers vous :)', {
+      theme: 'light',
+      type: 'success',
+      position: 'bottom-center',
+    });
+
+    setEmail('');
+    setMessage('');
+  };
+
+  return (
+    <div className={styles.container}>
+      <h1>Formulaire de contact</h1>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <label htmlFor="email">
+          Email
+          <input
+            type="email"
+            id="email"
+            className={styles.field}
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+
+        <label htmlFor="message">
+          Message
+          <textarea
+            id="message"
+            className={styles.field}
+            required
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+          />
+        </label>
+
+        <button type="submit" className={styles.submitBtn}>
+          Envoyer
+        </button>
+      </form>
+      <ToastContainer />
+    </div>
+  );
 }
