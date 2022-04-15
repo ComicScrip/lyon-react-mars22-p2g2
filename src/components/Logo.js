a7f14773ff554f8c807a4261dbd586f63a05f5aa
MyleneM49 <mylene.mercier.simplon@gmail.com>
2022-04-15 15:55:48 +0200

    MAJ + Animate CSS

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,10 +1,10 @@
-import './Logo.css';
+import '../index.css';
 import React from 'react';
 
 export default function Logo() {
   return (
     <div className="lettre">
-      <div className="recG w-1/3 h-16 bg-red-800">gauche</div>
+      <div className="recG w-1/3 h-16 bg-red-800 text-3xl">gauche</div>
       <div className="recM w-1/3 h-16bg-blue-900">milieu</div>
       <div className="recD w-1/3 h-16 bg-yellow-800">droite</div>
     </div>
--- src/tailwind.config.js
+++ src/tailwind.config.js
@@ -1,21 +1,7 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      // Configure your color palette here
-
-      extend: {},
-    },
+    extend: {},
   },
   plugins: [],
-};
-
-module.exports = {
-  theme: {
-    colors: {
-      tahiti: '#3ab7bf',
-      900: '#881337',
-      800: '#292524',
-    },
-  },
 };
