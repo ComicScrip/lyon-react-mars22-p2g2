1455b20fd7d0fbd68dab5fdb5b92a04a865236d0
JVRasa <j.rasamoelina@gmail.com>
2022-04-15 11:33:53 +0200

    finished profil page, desktop and mobile, just need to adjust the colors with the background img

--- src/components/ProfilDetail.js
+++ src/components/ProfilDetail.js
@@ -10,10 +10,24 @@ function ProfilDetail() {
 
         <section className={styles.profil}>
           <img src={profilPicture} alt="jeanne" className={styles.img} />
+          <div className={styles.profilName}>
+            <h1>Jeanne Profil</h1>
+          </div>
           <div className={styles.profilDescription}>
             <li>Prénom: Jeanne</li>
             <li>Age: 32 ans</li>
             <li>Catégories préférées : Action, aventure, thriller</li>
+          </div>
+          <div className={styles.profilBio}>
+            <div className={styles.profilBioScrollContainer}>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
+                blandit leo ac nulla finibus laoreet. Vestibulum molestie
+                iaculis dapibus. Curabitur non nunc ex. Integer magna enim,
+                elementum id sollicitudin vitae, tempus sit amet nibh. Sed
+                tristique nisi leo, at blandit metus scelerisque viverra.
+              </p>
+            </div>
           </div>
         </section>
       </div>
