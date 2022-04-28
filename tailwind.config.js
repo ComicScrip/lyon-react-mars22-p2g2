a6d50a62db14067babf2de91f0dcbf433c1e501d
MyleneM49 <mylene.mercier.simplon@gmail.com>
2022-04-28 16:47:10 +0200

    code mort enlevé.end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    colors: {
-      // green: '#13ce66',
-    },
-    extend: {
-      // fontSize: {
-      // body: '1rem',
-      // h1: '1,6rem',
-      // },
-    },
+    extend: {},
   },
 };
