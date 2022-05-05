517ba81b6cdc086c9cfd6af43e14516f95df4c5c
QuentinJOUANNE <QUENTIN.JOUANNE94@gmail.com>
2022-05-05 11:13:31 +0200

    update home style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -60,13 +60,11 @@ export default function Home() {
           <div className="borderMovie">
             <h2 className="movieMonthTitle">Movie of the month</h2>
           </div>
-          <a href="/detailpage">
-            <img
-              className="pictureOfMonth"
-              src="https://imdb-api.com/images/original/MV5BYTUyNjZkMjEtYmQ5OS00NzljLThiMmQtNWIwNTM1YjUyOWNjXkEyXkFqcGdeQXVyMTI0MzA4NTgw._V1_Ratio0.6837_AL_.jpg"
-              alt="filmofthemonth"
-            />
-          </a>
+          <img
+            className="pictureOfMonth"
+            src="https://imdb-api.com/images/original/MV5BYTUyNjZkMjEtYmQ5OS00NzljLThiMmQtNWIwNTM1YjUyOWNjXkEyXkFqcGdeQXVyMTI0MzA4NTgw._V1_Ratio0.6837_AL_.jpg"
+            alt="filmofthemonth"
+          />
           <div className="comunityContainer">
             <Button
               href="/nanarmate"
